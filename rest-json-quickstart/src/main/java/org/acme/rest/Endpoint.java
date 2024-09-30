e41b7af7fd880921aba27f67e392f72913f866c1
itsmealessandro <itsmealessandro@outlook.com>
2024-09-30 10:33:58 +0200

    [Q] idk

--- rest-json-quickstart/src/main/java/org/acme/rest/Endpoint.java
+++ rest-json-quickstart/src/main/java/org/acme/rest/Endpoint.java
@@ -10,4 +10,10 @@ public class Endpoint {
   public String hello() {
     return "jakarta and quarkus";
   }
+
+  @GET
+  @Path("hello")
+  public String helloGET() {
+    return "jakarta and quarkus from path";
+  }
 }
