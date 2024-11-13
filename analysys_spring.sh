df918cb71b892a1893fc10bbe02e8eec7492d329
itsmealessandro <itsmealessandro@outlook.com>
2024-11-13 15:26:26 +0100

    path change and test removed

--- analysys_spring.sh
+++ analysys_spring.sh
@@ -4,7 +4,8 @@ PATH_TO_LOCUST_FILE="${PATH_TO_LOCUST}/simulateSessions.py"
 PATH_TO_RESULTS="${PATH_TO_LOCUST}/csv_results"
 HOST="http://localhost:9966"
 
-time="" # in minutes
+# NOTE: input is expected in seconds, will be converted in minutes
+time=""  
 users=""
 db=""
 
